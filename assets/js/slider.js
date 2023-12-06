d2c373ff0576ffc0e11fe88557340b42895beec7
Gustavot24 <39109588+Gustavot24@users.noreply.github.com>
2023-12-05 22:06:06 -0300

    Add files via upload
    Innitial Commit

---
+++ assets/js/slider.js
@@ -0,0 +1,28 @@
+let slideIndex = 0;
+let slideIndex2 = 0;
+showSlides();
+showSlides2();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
+
+function showSlides2() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex2++;
+  if (slideIndex2 > slides.length) {slideIndex2 = 1}
+  slides[slideIndex2-1].style.display = "block";
+  setTimeout(showSlides2, 3000); // Change image every 2 seconds
+}
